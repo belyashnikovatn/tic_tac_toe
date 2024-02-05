fc72983633ad6be43c6f3e97195ac31afb2f1a18
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-02-05 16:21:16 +0500

    add one more Exception and Main Whlie

--- game.py
+++ game.py
@@ -1,49 +1,51 @@
 from gameparts import Board
-from gameparts.exceptions import FieldIndexError
+from gameparts.exceptions import CellOccupiedError, FieldIndexError
 
 
 def main():
     game = Board()
+    current_player = 'X'
+    running = True
     game.display()
 
-    # Запускаем бесконечный цикл
-    while True:
-        try:
-            row = input('Введите номер строки: ')
-            if any(map(str.isalpha, row)):
-                raise ValueError
+    while running:
+        print(f'Ход делает {current_player} игрок')
+
+        # Запускаем бесконечный цикл для ввода коор-т
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if not 0 <= row < game.field_size:
+                    raise FieldIndexError
+                col = int(input('Введите номер столбца: '))
+                if not 0 <= col < game.field_size:
+                    raise FieldIndexError
+                if game.board[row][col] != ' ':
+                    raise CellOccupiedError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}'
+                )
+                print('Введите значения для сторки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячйка занята')
+                print('Введите другие координаты')
+                continue
+            except ValueError:
+                print(
+                    'Буквы вводить нельзя. Только числа.',
+                    'Введите значения для строки и столбца заново.'
+                )
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
             else:
-                row = int(row)
-            if not 0 <= row < game.field_size:
-                raise FieldIndexError
-            col = input('Введите номер столбца: ')
-            if any(map(str.isalpha, col)):
-                raise ValueError
-            else:
-                col = int(col)
-            if not 0 <= col < game.field_size:
-                raise FieldIndexError
-        except ValueError:
-            print(
-                'Буквы вводить нельзя. Только числа.',
-                'Пожалуйста, введите значения для строки и столбца заново.'
-            )
-            continue
-        except FieldIndexError:
-            print(
-                'Значение должно быть неотрицательным и меньше '
-                f'{game.field_size}'
-            )
-            print('Пожалуйста, введите значения для сторки и столбца заново.')
-            continue
-        except Exception as e:
-            print(f'Возникла ошибка: {e}')
-            continue
-        else:
-            break
-    game.make_move(row, col, 'x')
-    print('ход сделан!')
-    game.display()
+                break
+        game.make_move(row, col, current_player)
+        game.display()
+        current_player = 'O' if current_player == 'X' else 'X'
 
 
 if __name__ == '__main__':
